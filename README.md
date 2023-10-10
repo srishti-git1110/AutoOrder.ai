8f987b44eae836a8ef3c029d4bc1a16344303465
srishti-git1110 <srishtigureja1110@gmail.com>
2023-10-10 21:26:42 +0530

    fixed bugs

--- README.md
+++ README.md
@@ -3,5 +3,10 @@ A FastAPI endpoint that takes an input image (a chat screenshot, a food/clothing
 It is able to automate actions like scheduling meetings based on a chat's ss, ordering a food dish by only using its image etc.
 
 ## Usage
-To run the API
+To run the API, clone the repository and run the following in the terminal -
+```
+pip install uvicorn
+uvicorn app:app --reload
+
+```
 
--- test_api.py
+++ test_api.py
@@ -1,9 +1,26 @@
+import argparse
+import os
 import requests
 
-url = 'http://127.0.0.1:8000/process-image'
-files = {'img': ('pizza.jpg', open('C:/Users/hp/Downloads/pizza.jpg', 'rb'))}
+def main():
+    parser = argparse.ArgumentParser(description='Test the FastAPI API with an image file.')
+    parser.add_argument('image_path', type=str, help='Path to the image file on your local system')
+    args = parser.parse_args()
 
-response = requests.post(url, files=files)
+    if not os.path.exists(args.image_path):
+        print(f"Error: The file '{args.image_path}' does not exist.")
+        return
 
-print(response.status_code)
-print(response.json())
+    url = 'http://127.0.0.1:8000/process-image'
+    files = {'img': (os.path.basename(args.image_path), open(args.image_path, 'rb'))}
+
+    response = requests.post(url, files=files)
+
+    if response.status_code == 200:
+        print("API response:")
+        print(response.json())
+    else:
+        print(f"API request failed with status code {response.status_code}")
+
+if __name__ == "__main__":
+    main()
