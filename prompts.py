cb4694f33ab47c6e36708e211f432d92e6c1d371
Srishti Gureja <84127491+srishti-git1110@users.noreply.github.com>
2023-10-16 22:28:10 +0530

    Update prompts.py

--- prompts.py
+++ prompts.py
@@ -21,8 +21,8 @@ Your instruction: Order a pizza with various toppings, including vegetables, che
 The swimsuit is designed for young girls and is made of a soft, stretchy material that allows for a comfortable fit and ease of movement. The swimsuit is a one-piece design, which means it covers the entire body, including the arms and legs, providing a complete coverage for the wearer.
 Your instruction: Order a pink one-piece swimsuit made of a soft, stretchy material.
 
-3. image description: The image shows a chat screenshot between two people who are discussing a meeting time fir for both of them. First user proposed 6pm tuesday as a suitable time to which the second user agreed, and asked if herumbshandilya123@gmail.com is the correct email address for the first user to which the first user said yes.
-Your instruction: Schedule a meeting for 6pm this tuesday at the address herumbshandilya123@gmail.com.
+3. image description: The image shows a chat screenshot between two people who are discussing a meeting time for for both of them. First user proposed 6pm tuesday as a suitable time to which the second user agreed, and asked if srishtigureja1110@gmail.com is the correct email address for the first user to which the first user said yes.
+Your instruction: Schedule a meeting for 6pm this tuesday at the address srishtigureja1110@gmail.com.
 The instructions should cover all of the details about the product that are described in the image description, and should also dictate a human to do a ceratin thing using verbs like 'order [food details]', 'schedule a meeting [meeting details]', etc. so that they they ask a human to follow a certain action.
 Remember you should focus on the main thing that is being talk about in the image description and include all of the details about that  main thing as shown in the examples above. 
 Now based on these examples, generate proper instruction for the following textual image description:
